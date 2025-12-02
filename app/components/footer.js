59fee118111b005745f25af33e9f2e16dea44ea6
johnemmanuelpieza-ops <johnemmanuel.pieza@wvsu.edu.ph>
2025-12-02 12:54:53 +0800

    Update footer.js

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-6 mt-8 flex justify-center items-center">
-      <p className="text-sm hover:text-gray-400 transition duration-300">My EMM. All rights reserved.</p>
+      <p className="text-sm hover:text-gray-400 transition duration-300">NBA 2K. All rights reserved.</p>
     </footer>
   );
 }
